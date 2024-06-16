1b1f84d24ee2731d9a810a07ad39b3484683b55b
sidiqslenses <sidiqslenses@gmail.com>
2024-06-16 08:47:18 +0000

    new mod

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+"use client" 
+
+import React, { Suspense } from 'react'
+
+function AboutContent() {
+  return (
+    <div className='m-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae et nemo repellat recusandae sunt distinctio nulla blanditiis? Consequatur rem at nam facere ipsum est dolor voluptas modi, tenetur impedit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae et nemo repellat recusandae sunt distinctio nulla blanditiis? Consequatur rem at nam facere ipsum est dolor voluptas modi, tenetur impedit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae et nemo repellat recusandae sunt distinctio nulla blanditiis? Consequatur rem at nam facere ipsum est dolor voluptas modi, tenetur impedit.</p>
+    </div>
+  )
+}
 
 export default function About() {
   return (
-    <div className='m-w-6xl mx-auto p-3 space-y-4'>
-        <h1 className='text-2xl font-medium text-amber-600'>
-            About
-        </h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae et nemo repellat recusandae sunt distinctio nulla blanditiis? Consequatur rem at nam facere ipsum est dolor voluptas modi, tenetur impedit.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae et nemo repellat recusandae sunt distinctio nulla blanditiis? Consequatur rem at nam facere ipsum est dolor voluptas modi, tenetur impedit.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi molestiae et nemo repellat recusandae sunt distinctio nulla blanditiis? Consequatur rem at nam facere ipsum est dolor voluptas modi, tenetur impedit.</p>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <AboutContent />
+    </Suspense>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,29 @@
-const API_KEY = process.env.API_KEY
-
 import Results from '@/components/Results';
-import React from 'react'
+import React from 'react';
 
-export default async function Home({searchParams}) {
-  const genre = searchParams.genre || 'fetchTrending'
-  const res = await fetch(`
-  https://api.themoviedb.org/3${
-    genre === 'fetchTopRated' ?
-     `/movie/top_rated` : `/trending/all/week`
-}?api_key=${API_KEY}&language=en-US&page=1`);
-  const data =await res.json()
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  let url;
+
+  if (genre === 'fetchTopRated') {
+    url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
+  } else {
+    url = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`;
   }
-  const results = data.results 
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  const results = data.results;
 
   return (
-  <div>
-    <Results results={results}/>
-  </div>
-    )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
